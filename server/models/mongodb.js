96dc7246853b93e8d26f8f07bacfe42495e5cd5e
sravan kumar <sravandasari111@gmail.com>
2019-08-02 22:41:58 +0530

    updated

--- server/models/mongodb.js
+++ server/models/mongodb.js
@@ -15,29 +15,7 @@ mongoose.connection.on('error',function(err){
     console.log('Mongoose connection error:' + err);
 });
 
-/*mongoose.connection.on('disconnected', function(){
-    console.log('Mongoose disconnected');
-});
 
-shutdown = function(msg,callback) {
-    mongoose.connection.close(function(){
-        console.log('Mongoose disconnected through '+ msg);
-    });
-};
-
-process.once('SIGUSR2', function(){
-    shutdown('nodemon restart', function(){
-        process.kill(process.pid,'SIGUSR2');
-    });
-
-});
-
-process.on('SIGINT', function(){
-    shutdown('app termination',function(){
-        process.exit(0);
-    });
-});
-*/
 require('./admin');
 require('./class');
 require('./marks');
